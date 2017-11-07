35ccee6c978f61b68f5f4e1e2d511770a0a9ed03
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-07 13:05:03 +0100

    Add basic `x-partial` AST transform

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* eslint-env node */
+
 'use strict';
 
+const buildAstTransform = require('./lib/ast-transform');
+
+const name = 'ember-non-context-shifting-partials';
+
 module.exports = {
-  name: 'ember-non-context-shifting-partials'
+  name,
+
+  setupPreprocessorRegistry(type, registry) {
+    if (type === 'parent') {
+      registry.add('htmlbars-ast-plugin', {
+        name,
+        plugin: buildAstTransform(this),
+        baseDir() { return __dirname; }
+      });
+    }
+  },
 };
